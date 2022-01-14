e66b2a2c1ed4c136475786a95c656bc797315279
Antonio-Devoluti <raymendesjr2013@gmail.com>
2022-01-14 15:36:54 -0300

    rm: rollback from users and tokens

--- database/migrations/1636151433180_users.ts
+++ database/migrations/1636151433180_users.ts
@@ -4,21 +4,23 @@ export default class Users extends BaseSchema {
   protected tableName = 'users'
 
   public async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.bigIncrements('id')
-      table.string('username').unique().notNullable()
-      table.string('fullname')
-      table.string('photo')
-      table.string('password')
-      table.float('savings').defaultTo(0)
-      table.enu('level', [0, 1, 2, 3])
+    ;(await !this.schema.hasTable(this.tableName))
+      ? this.schema.createTable(this.tableName, (table) => {
+          table.bigIncrements('id')
+          table.string('username').unique().notNullable()
+          table.string('fullname')
+          table.string('photo')
+          table.string('password')
+          table.float('savings').defaultTo(0)
+          table.enu('level', [0, 1, 2, 3])
 
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
-    })
+          table.timestamp('created_at', { useTz: true })
+          table.timestamp('updated_at', { useTz: true })
+        })
+      : this.schema.alterTable(this.tableName, (table) => {})
   }
 
   public async down() {
-    this.schema.dropTable(this.tableName)
+    // this.schema.dropTable(this.tableName)
   }
 }
--- database/migrations/1636151433181_api_tokens.ts
+++ database/migrations/1636151433181_api_tokens.ts
@@ -4,22 +4,29 @@ export default class ApiTokens extends BaseSchema {
   protected tableName = 'api_tokens'
 
   public async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.bigIncrements('id').primary()
-      table.bigInteger('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
-      table.string('name').notNullable()
-      table.string('type').notNullable()
-      table.string('token', 64).notNullable().unique()
+    ;(await !this.schema.hasTable(this.tableName))
+      ? this.schema.createTable(this.tableName, (table) => {
+          table.bigIncrements('id').primary()
+          table
+            .bigInteger('user_id')
+            .unsigned()
+            .references('id')
+            .inTable('users')
+            .onDelete('CASCADE')
+          table.string('name').notNullable()
+          table.string('type').notNullable()
+          table.string('token', 64).notNullable().unique()
 
-      /**
-       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
-       */
-      table.timestamp('expires_at', { useTz: true }).nullable()
-      table.timestamp('created_at', { useTz: true }).notNullable()
-    })
+          /**
+           * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
+           */
+          table.timestamp('expires_at', { useTz: true }).nullable()
+          table.timestamp('created_at', { useTz: true }).notNullable()
+        })
+      : this.schema.alterTable(this.tableName, (table) => {})
   }
 
   public async down() {
-    this.schema.dropTable(this.tableName)
+    // this.schema.dropTable(this.tableName)
   }
 }
